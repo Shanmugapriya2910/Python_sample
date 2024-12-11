ab53ead0c8af60b304113f65d6e364ba89458575
Shanmugapriya <103830454+Shanmugapriya2910@users.noreply.github.com>
2024-12-11 08:34:40 +0000

    Update oddevenprime.py

--- oddevenprime.py
+++ oddevenprime.py
@@ -9,7 +9,7 @@ def numbers(start, end):
     def is_prime(num):
         if num < 2:
             return False
-        for i in range(2, int(math.sqrt(num)) + 1):
+        for i in range(2, num):
             if num % i == 0:
                 return False
         return True
